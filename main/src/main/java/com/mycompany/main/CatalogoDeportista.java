5b69eb7c4755e0e3bf59d5368dd87309959a50dd
Vibexz0706 <111944492+Vibexz0706@users.noreply.github.com>
2022-11-19 16:02:00 -0600

    commit vibexz

---
+++ main/src/main/java/com/mycompany/main/CatalogoDeportista.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author VIBEXZ
+ */
+public class CatalogoDeportista extends HERENCIA {
+    
+    private String identificacion; // LO CREO STRING PORQUE PUEDE SER UNA PERSONA EXTRANJERA
+    
+    
+    
+}
