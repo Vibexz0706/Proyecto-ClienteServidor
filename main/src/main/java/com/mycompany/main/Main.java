632e9fccd21f3609a47f376524ea8fce802a3ea0
johan527 <johanmg41@gmail.com>
2022-11-19 15:51:34 -0600

    avance1

--- main/src/main/java/com/mycompany/main/Main.java
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.main;
 
 /**
  *
- * @author VIBEXZ
+ * @author Vibez Y Johan
  */
 public class Main {
 
